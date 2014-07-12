5275e77e2c1b47ff1e7da8b87fccb0a5af498205
Guilherme Avila <guilhermebav@gmail.com>
2014-07-12 05:41:55 -0300

    Updating public view according to lado9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,29 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 	before_action :configure_devise_permitted_parameters, if: :devise_controller?
+	layout :layout_by_resource
 
+protected
 
-	protected
+	def layout_by_resource
+		if devise_controller? && !user_signed_in?
+			"devise"
+		else
+			"application"
+		end
+	end
 
-	  def configure_devise_permitted_parameters
-	    registration_params = [:name, :email, :password, :password_confirmation, :approved, :admin]
+	def configure_devise_permitted_parameters
+		registration_params = [:name, :email, :password, :password_confirmation, :approved, :admin]
 
-	    if params[:action] == 'update'
-	      devise_parameter_sanitizer.for(:account_update) { 
-	        |u| u.permit(registration_params << :current_password)
-	      }
-	    elsif params[:action] == 'create'
-	      devise_parameter_sanitizer.for(:sign_up) { 
-	        |u| u.permit(registration_params) 
-	      }
-	    end
-	  end
-
+		if params[:action] == 'update'
+			devise_parameter_sanitizer.for(:account_update) { 
+			|u| u.permit(registration_params << :current_password)
+			}
+		elsif params[:action] == 'create'
+			devise_parameter_sanitizer.for(:sign_up) { 
+				|u| u.permit(registration_params) 
+			}
+		end
+	end
 end
